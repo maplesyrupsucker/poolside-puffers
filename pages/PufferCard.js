fc4f1590ef2231046110d341bdd6bef7112e5dd1
Corbin <maplesyrupsucker@protonmail.com>
2021-10-08 21:12:20 +0700

    pureblood and halfblood counter
    Still broken, but getting there

--- pages/PufferCard.js
+++ pages/PufferCard.js
@@ -71,10 +71,20 @@ export default function PufferCard({ pufferContract, walletAddress }) {
     }
   }, []);
 
+  function numberOfPureBloods(attributes) {
+
+  }
+
+  function numberOfHalfBloods(attributes) {
+
+  }
+
   function numberOfDiamonds(attributes) {
     let diamondCount = 0;
+    let halfBloodCount = 0;
+    let pureBloodCount = 0;
 
-    //if PUREBLOOOOOD +1 diamon ************
+    //setup purebloods
     const purebloodRed =
       attributes.filter((i) => i.value === "Red").length >= 5;
     const purebloodYellow =
@@ -90,6 +100,126 @@ export default function PufferCard({ pufferContract, walletAddress }) {
     const purebloodViolet =
       attributes.filter((i) => i.value === "Violet").length >= 5;
 
+    // BODY PARTS THAT IMPACT BLOOD
+    // body
+    // spikes
+    // fins
+    // tail
+    // tail fins 
+
+    //setup half breeds
+    const fourRed =
+      attributes.filter((i) => i.value === "Red").length == 4;
+    const fourOrange = 
+      attributes.filter((i) => i.value === "Orange").length == 4;
+    const fourYellow =
+      attributes.filter((i) => i.value === "Yellow").length == 4;
+    const fourGreen =
+      attributes.filter((i) => i.value === "Green").length == 4;
+    const fourBlue =
+      attributes.filter((i) => i.value === "Blue").length == 4;
+    const fourIndigo =
+      attributes.filter((i) => i.value === "Indigo").length == 4;
+    const fourViolet =
+      attributes.filter((i) => i.value === "Violet").length == 4;
+
+
+    const threeRed =
+      attributes.filter((i) => i.value === "Red").length == 3;
+    const threeOrange = 
+      attributes.filter((i) => i.value === "Orange").length == 3;
+    const threeYellow =
+      attributes.filter((i) => i.value === "Yellow").length == 3;
+    const threeGreen =
+      attributes.filter((i) => i.value === "Green").length == 3;
+    const threeBlue =
+      attributes.filter((i) => i.value === "Blue").length == 3;
+    const threeIndigo =
+      attributes.filter((i) => i.value === "Indigo").length == 3;
+    const threeViolet =
+      attributes.filter((i) => i.value === "Violet").length == 3;
+    
+    const twoRed =
+      attributes.filter((i) => i.value === "Red").length == 2;
+    const twoOrange = 
+      attributes.filter((i) => i.value === "Orange").length == 2;
+    const twoYellow =
+      attributes.filter((i) => i.value === "Yellow").length == 2;
+    const twoGreen =
+      attributes.filter((i) => i.value === "Green").length == 2;
+    const twoBlue =
+      attributes.filter((i) => i.value === "Blue").length == 2;
+    const twoIndigo =
+      attributes.filter((i) => i.value === "Indigo").length == 2;
+    const twoViolet =
+      attributes.filter((i) => i.value === "Violet").length == 2;
+
+
+
+if (
+
+    fourRed ||
+    fourOrange || 
+    fourYellow ||
+    fourGreen ||
+    fourBlue ||
+    fourIndigo ||
+    fourViolet ||
+
+    threeRed && twoOrange == true ||
+    threeRed && twoYellow == true ||
+    threeRed && twoGreen == true ||
+    threeRed && twoBlue == true ||
+    threeRed && twoIndigo == true ||
+    threeRed && twoViolet == true ||
+
+    threeOrange && twoRed == true ||
+    threeOrange && twoYellow == true ||
+    threeOrange && twoGreen == true ||
+    threeOrange && twoBlue == true ||
+    threeOrange && twoIndigo == true ||
+    threeOrange && twoViolet == true || 
+    
+    threeYellow && twoOrange == true ||
+    threeYellow && twoRed == true ||
+    threeYellow && twoGreen == true ||
+    threeYellow && twoBlue == true ||
+    threeYellow && twoIndigo == true ||
+    threeYellow && twoViolet == true ||
+
+    threeGreen && twoOrange == true ||
+    threeGreen && twoRed == true ||
+    threeGreen && twoYellow == true ||  
+    threeGreen && twoBlue == true ||
+    threeGreen && twoIndigo == true ||
+    threeGreen && twoViolet == true ||
+
+    threeBlue && twoOrange == true ||
+    threeBlue && twoRed == true ||
+    threeBlue && twoYellow == true ||
+    threeBlue && twoGreen == true ||
+    threeBlue && twoIndigo == true ||
+    threeBlue && twoViolet == true ||
+
+    threeIndigo && twoOrange == true ||
+    threeIndigo && twoRed == true ||
+    threeIndigo && twoYellow == true ||
+    threeIndigo && twoGreen == true ||
+    threeIndigo && twoBlue == true ||
+    threeIndigo && twoViolet == true ||
+
+    threeViolet && twoOrange == true ||
+    threeViolet && twoRed == true ||
+    threeViolet && twoYellow == true ||
+    threeViolet && twoGreen == true ||
+    threeViolet && twoBlue == true ||
+    threeViolet && twoIndigo == true 
+  ) {
+    diamondCount += 1;
+    halfBloodCount += 1;
+  }
+
+    // if purebloods
     if (
       purebloodBlue ||
       purebloodGreen ||
@@ -100,6 +230,7 @@ export default function PufferCard({ pufferContract, walletAddress }) {
       purebloodYellow
     ) {
       diamondCount += 2;
+      pureBloodCount += 1;
     }
 
     attributes.forEach((attribute) => {
@@ -112,7 +243,11 @@ export default function PufferCard({ pufferContract, walletAddress }) {
       if (attribute.value.indexOf("Animated Pool") >= 0) diamondCount += 1;
     });
     console.log("diamondcount", diamondCount);
+    console.log("purebloodCount", pureBloodCount);
+    console.log("halfBloodCount", halfBloodCount);
 
+    // return pureBloodCount;
+    // return halfBloodCount;
     return diamondCount; // return a number
   }
 
@@ -134,7 +269,15 @@ export default function PufferCard({ pufferContract, walletAddress }) {
       {pufferCardData.map((obj) => {
         const { attributes } = obj;
         const diamondCount = numberOfDiamonds(attributes);
+
+        const pureBloodCount = numberOfDiamonds(attributes);
+        const halfBloodCount = numberOfDiamonds(attributes);
+
         const diamondArray = new Array(diamondCount).fill("");
+
+        const pureBloodArray = new Array(pureBloodCount).fill("");
+        const halfBloodArray = new Array(halfBloodCount).fill("");
+
         /*  for (let i = 0; i < diamondCount; i++) {
           diamondArray.push("");
         }
@@ -150,6 +293,24 @@ export default function PufferCard({ pufferContract, walletAddress }) {
                       alt="rare"
                       className="diamond"
                     ></img>
+                  );
+                })}
+                {pureBloodArray.map((attributes) => {
+                  return (
+                    <img
+                        src="images/pureblood.png"
+                        alt="pure blood"
+                        className="diamond"
+                      ></img>
+                  );
+                })}
+                {halfBloodArray.map((attributes) => {
+                  return (
+                    <img
+                        src="images/halfblood.png"
+                        alt="half blood"
+                        className="diamond"
+                      ></img>
                   );
                 })}
               </span>
